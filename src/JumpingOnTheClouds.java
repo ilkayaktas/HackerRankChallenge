6a3bcac18c5c7c186184671e29e8f332616c0758
İlkay Aktaş <ilkayaktas@gmail.com>
2018-12-14 16:24:31 +0300

    Jumping On The Clouds

---
+++ src/JumpingOnTheClouds.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aselsan on 12.12.2018 at 16:14.
+ */
+
+public class JumpingOnTheClouds {
+    static int jumpingOnClouds(int[] c) {
+
+        return 0;
+    }
+}
---
+++ test/JumpingOnTheCloudsTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by aselsan on 12.12.2018 at 16:14.
+ */
+public class JumpingOnTheCloudsTest {
+
+    @Test
+    public void jumpingOnClouds() {
+        JumpingOnTheClouds.jumpingOnClouds(new int[]{0, 0, 0, 0, 1, 0});
+    }
+}
