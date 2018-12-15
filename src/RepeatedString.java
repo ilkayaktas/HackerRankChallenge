d7ca5b9c9128dace7c0c1a20952385486a451e07
ilkayaktas <ilkayaktas@gmail.com>
2018-12-15 13:26:56 +0300

    RepeatedString

--- src/RepeatedString.java
+++ src/RepeatedString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by ilkayaktas on 2018-12-15 at 12:58.
  */
@@ -5,15 +7,15 @@
 public class RepeatedString {
 
     static long repeatedString(String s, long n) {
-        long total = 0;
-        for (long i = 0; i < n;) {
-            String [] sArr = s.split("");
-            for (int j = 0; j < sArr.length && i < n; j++, i++) {
-                if (sArr[j].equals("a")){
-                    total++;
-                }
-            }
+        long numOfA = Arrays.asList(s.split("")).stream().filter(s1 -> s1.equals("a")).count();
+
+        long numOfStr = n/s.length();
+        long rest = n%s.length();
+        long totalNumOfA = numOfA*numOfStr;
+        if(rest != 0){
+            totalNumOfA += Arrays.asList(s.substring(0, (int)rest).split("")).stream().filter(s1 -> s1.equals("a")).count();
         }
-        return total;
+
+        return totalNumOfA;
     }
 }
